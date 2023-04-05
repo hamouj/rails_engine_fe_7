2e24ea575573a5c6359bb44836475f8ebecc83eb
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-05 13:42:23 -0700

    Feat: create #merchant_items() method and tests

--- app/services/rails_engine_service.rb
+++ app/services/rails_engine_service.rb
@@ -4,6 +4,10 @@ class RailsEngineService
     get_url('/api/v1/merchants')
   end
 
+  def merchant_items(merchant_id)
+    get_url("/api/v1/merchants/#{merchant_id}/items")
+  end
+
   def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
--- spec/services/rails_engine_service_spec.rb
+++ spec/services/rails_engine_service_spec.rb
@@ -18,5 +18,22 @@ describe RailsEngineService do
         expect(merchant[:attributes][:name]).to be_a String
       end
     end
+
+    context '#merchant_items()' do
+      it 'returns all items for a specific merchant' do
+        merchant_items = RailsEngineService.new.merchant_items(1)
+
+        expect(merchant_items).to be_a Hash
+        expect(merchant_items[:data]).to be_an Array
+
+        item = merchant_items[:data].first
+
+        expect(item).to have_key :id
+        expect(item[:id]).to be_a String
+
+        expect(item[:attributes]).to have_key :name
+        expect(item[:attributes][:name]).to be_a String
+      end
+    end
   end
 end
